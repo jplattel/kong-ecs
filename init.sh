05ef2bd439da2cc9406a8008dc2470c9b15210fa
Wouter Danes <wouter.danes@gmail.com>
2017-02-26 13:12:49 +0100

    Only doing host IP, port advertise doesn't work.

--- init.sh
+++ init.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 set -eu
-docker > /dev/null
-[ -e /var/run/docker.sock ]
 
-HOST_IP=$(curl -s 169.254.169.254/1.0/meta-data/local-ipv4)
-OUTER_PORT=$(docker inspect -f '{{index .NetworkSettings.Ports "7946/tcp" 0 "HostPort"}}' $(hostname))
+host_ip=$(curl -s 169.254.169.254/1.0/meta-data/local-ipv4)
 
-KONG_CLUSTER_ADVERTISE="$HOST_IP:$OUTER_PORT" kong start
+KONG_CLUSTER_ADVERTISE="$host_ip" kong start
